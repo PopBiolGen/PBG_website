02ab8ec4f456c38594f240abb61bd7061b2246a5
Ben Phillips <phillipsb@unimelb.edu.au>
2023-07-06 10:16:17 +0800

    Reverse sort publications by date, add formatting for book sections.

--- assets/pubs/formatPubs.R
+++ assets/pubs/formatPubs.R
@@ -3,6 +3,10 @@ library(knitr)
 
 bib_data <- ReadBib("assets/pubs/PBG_pubs.bib")
 
+# sort the bibliography
+bib_data <-  sort(bib_data, sorting = "ynt", decreasing = TRUE)
+
+
 # function to output markdown format
   # takes a single reference as argument
   # designed to be used with lapply
@@ -13,13 +17,16 @@ bib2md <- function(bib.entry){
   }
   auth.string <- lapply(bib.entry$author, author)
   auth.string <- paste(unlist(auth.string), collapse = ", ")
+  # remove annoying curly braces
+  title <- gsub("\\{", "", bib.entry$title)
+  title <- gsub("\\}", "", title)
   
   if (bib.entry$bibtype == "Article"){
     paste(auth.string,
           " (",
           bib.entry$year,
           ").  ",
-          bib.entry$title,
+          title,
           "  *",
           bib.entry$journal,
           "*. **",
@@ -29,29 +36,29 @@ bib2md <- function(bib.entry){
           ".",
           "\n\n ", 
           sep="")
+  } else
+  if (bib.entry$bibtype == "InCollection" || bib.entry$bibtype == "InProceedings"){
+    eds <- lapply(bib.entry$editor, author)
+    eds <- paste(unlist(eds), collapse = ", ")
+    paste(auth.string,
+          " (",
+          bib.entry$year,
+          ").  ",
+          title,
+          "  in ",
+          eds,
+          " *",
+          bib.entry$book.title,
+          "*. ",
+          bib.entry$publisher,
+          ". ",
+          bib.entry$place,
+          ".",
+          "\n\n ",
+          sep="")
   }
-  
-  # if (bib.entry$bibtype == "Book Section"){
-  #   eds <- lapply(bib.entry$editor, author)
-  #   eds <- paste(unlist(eds), collapse = ", ")
-  #   paste(auth.string,
-  #         " (",
-  #         bib.entry$year,
-  #         ").  ",
-  #         bib.entry$title,
-  #         "  in ",
-  #         eds,
-  #         " *",
-  #         bib.entry$book.title,
-  #         "*. ",
-  #         bib.entry$publisher,
-  #         ". ",
-  #         bib.entry$place,
-  #         ".",
-  #         "\n\n ", 
-  #         sep="")
-  # }
 }
+
 
 # format the bibliography
 formatBib <- lapply(bib_data, bib2md)

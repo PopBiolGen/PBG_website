c292ee1ba22f7ef2c518477882605abf3b0792ce
Ben Phillips <ben.l.phillips@curtin.edu.au>
2023-08-02 21:09:15 +0800

    Add pubs to woylie project

--- assets/pubs/projectPubs.R
+++ assets/pubs/projectPubs.R
@@ -3,8 +3,8 @@
 library(RefManageR)
 library(knitr)
 
-bibFile <- "assets/pubs/projectRefs/daphnia.bib"
-projFile <- "_projects/2016-01-01-daphnia.md"
+bibFile <- "assets/pubs/projectRefs/antipredbehav.bib"
+projFile <- "_projects/2021-01-01-AntipredatorBehaviour.md"
 
 #read data
 bib_data <- ReadBib(bibFile)
